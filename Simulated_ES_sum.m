ec1120868af684222e49e09e71209806fcd277ab
ldkr00 <lukasdekker77@gmail.com>
2022-10-11 11:39:25 +0200

    Changed functions for ES simulation

--- Simulated_ES_sum.m
+++ Simulated_ES_sum.m
@@ -1,6 +1,6 @@
 % Function for Question 5, simulated ES for sum of two stable r.v.'s
 
-function ES_sum_sim = Simulated_ES(nobs, a1, a2, b, c, d, xi, seed)
+function ES_sum_sim = Simulated_ES_sum(nobs, a1, a2, b, c, d, xi, seed)
 nobs = 1e6;
 X1 = stabgen(nobs, a1, b, d, c, seed); X2 = stabgen(nobs, a2, b, d, c, seed); S = X1 + X2;
 q = quantile(S, xi);
