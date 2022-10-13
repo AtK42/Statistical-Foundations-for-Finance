af72020b20aa4717bd09639b22337c7bb654c697
AtK42 <aaron_7@live.de>
2022-10-13 16:47:30 +0200

    corrected ex 3.2

--- assignment1.m
+++ assignment1.m
@@ -69,10 +69,10 @@ saveas(gca, 'assignment1_ex2.png')
 % alpha is given as alpha1 = a1 = 1.6 and alpha2 = a2 = 1.8
 % beta, scale and location are the same for both, i.e.
 % beta=b=b1=b2=0 
-% scale=c=c1=c2=1 
+% scale=c=c1=c2=1
 % location=d=d1=d2=0
-a1 = 1.6; a2 = 1.8; b = 0; c = 1; d = 0;
-n = 4*1e4; xvec = -10:.05:10;
+a1 = 1.5; a2 = 1.9; b = 0; c = 1; d = 0;
+n = 4*1e2; xvec = -10:0.05:10;
 svec=-10:0.05:10;
 
 
@@ -87,18 +87,16 @@ svec=-10:0.05:10;
 % however many you can do, i.e., maybe it takes too long, and you only do
 % 100 points --- be smart, and understand what we are doing here.)
 f = convopdf(svec,a1,a2);
-figure, plot(svec, f, 'linewidth', 3)
+figure, plot(svec, f, 'g-', 'linewidth', 3)
 
 %see slide "Asymmetric Stable: P.D.F. Calculation" (s. 553)
-%figure, plot(-20:20, repelem(0, 41), 'g-', 'linewidth', 3) %place holder
-%xlim([-20 20])
 
 % #2 Next, you compute the pdf by using the inversion formula applied to 
 % the characteristic function of the sum of X1 and X2, which is, remember,
 % the product of the two characteristic functions. Do it, plot it. Overlay
 % the two lines. They should be nearly identical.
 theostab_conv_ex3_2 = asymstabplus_ex3_2(xvec, a1, a2);
-hold on, plot(xvec, theostab_conv_ex3_2, 'b-', 'linewidth', 3), hold off
+hold on, plot(xvec, theostab_conv_ex3_2, 'b--', 'linewidth', 3), hold off
 
 % #3 Add a third line to your graphic, based on simulation and kernel 
 % density.
@@ -107,8 +105,8 @@ randstab_conv_a1 = stabgen(n, a1, b, c, d, 5);
 randstab_conv_a2 = stabgen(n, a2, b, c, d, 7585);
 randstab_conv_s = randstab_conv_a1 + randstab_conv_a2;
 [f_conv_a, x_conv_a] = ksdensity(randstab_conv_s,svec);
-hold on, plot(x_conv_a, f_conv_a, 'r--', 'linewidth', 3), hold off
- 
+hold on, plot(x_conv_a, f_conv_a, 'r:', 'linewidth', 3), hold off
+
 % Obviously, lavishly annotate your graphic with titles, x and y labels,
 % a legend, make the x and y numbers on the axis be big enough (use in
 % Matlab set(gca,'fontsize',16)), etc. Use nice clear colors for each of
@@ -118,11 +116,11 @@ hold on, plot(x_conv_a, f_conv_a, 'r--', 'linewidth', 3), hold off
 legend('PDF by convolution formula', ...
        'PDF by inversion formula', ...
        'PDF by simulation', ...
-       'Location', 'NorthWest')
+       'Location', 'southoutside', 'Orientation', 'vertical', 'Box', 'off')
 title('PDF For A Convolution of two Stable Distribution r.v.s with different \alpha')
 xlabel("x"); ylabel("S(x)")
 set(gca, 'fontsize', 10)
-%saveas(gca, 'assignment1_ex3.png'
+saveas(gca, 'assignment1_ex3_2.png')
 
 %% Question 4
 a = 1.7; b = 0; c = 0; d = 1; xi = 0.01;
