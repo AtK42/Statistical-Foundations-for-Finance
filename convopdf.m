bda3b4f21835953e2736b0985f522445040ad4ef
cannicj <jannicnoel.cavegn@uzh.ch>
2022-10-17 14:41:53 +0200

    comments that describe the two functions briefly

--- convopdf.m
+++ convopdf.m
@@ -1,4 +1,6 @@
 function f = convopdf(svec, a1, a2)
+%function that integrates the convointegrand function at different values
+%of s (given as input: svec) with respect to x
 xl=length(svec); f=zeros(xl, 1);
 for loop=1:length(svec)
     s=svec(loop);
