18fad1129956e1524818cc40610c1755073e7348
cannicj <jannicnoel.cavegn@uzh.ch>
2022-12-07 20:34:04 +0100

    no changes

--- assignment3/corrmat.m
+++ assignment3/corrmat.m
@@ -1,5 +1,7 @@
 function B = corrmat(d)
 % function to create correlation matrix (positive definite)
+% works quite well for low dimensions (until 7 dimensions), otherwise it 
+% takes a while
 r = -1 + (1+1)*rand(d);
 M = tril(r,-1);
 B = M + M' + eye(d);
@@ -8,6 +10,7 @@ while all(eig(B) > 0) == 0
     r = -1 + (1+1)*rand(d);
     M = tril(r,-1);
     B = M + M' + eye(d);
+    disp(1)
 end
 
 
